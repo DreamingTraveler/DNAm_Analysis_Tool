2d06e823d68c70bf322636d0c49deb8b1f75de27
Daniel <dreaming34523@gmail.com>
2021-08-13 22:53:39 +0800

    chore: add csv file path to config

--- default_config.py
+++ default_config.py
@@ -3,5 +3,9 @@ SECRET_KEY = 'c882369d7b66e8697426c3e8fb2675c0'
 
 SUB_DF_COLUMNS = ["Probe_ID", "logFC", "t", "P.Value", "CHR", "MAPINFO", "gene", "feat.cgi"]
 
-COLORECTAL_DMP_CSV = "static/data/DMP_analysis_result_TN.csv"
+# colorectal cancer DNAm analysis result files
+COLORECTAL_DMP_CSV = "static/data/colorectal_DMP_analysis_result_TN.csv"
 COLORECTAL_BIOMARKERS_CSV = "static/data/colorectal_TN_biomarkers_threshold_0.5.csv"
+
+# bladder
+BLADDER_DMP_CSV = "static/data/bladder_DMP_analysis_result_TN.csv"
